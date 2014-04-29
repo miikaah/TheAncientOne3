21fa352f7273f7dfc4373fbabf20085db4d01b61
letteralex <letteralex@hotmail.com>
2014-04-29 13:00:27 +0300

    Allu 29-04
    Liikkumiseen pieni parannus kävelee nyt järkevää vauhtia ja liikkuu
    sillei ylös ja alas kävellessä "luontevasti".

---
+++ Assets/Scripts/god_movement_updown.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+private var state: int;
+var object: Transform;
+
+function Start () {
+	state = 1;
+}
+
+function Update () {
+	if (Input.GetAxis("Horizontal") >=0.1 || Input.GetAxis("Horizontal") <=-0.1){
+		if(state == 1){
+			object.transform.localPosition.y -= 0.005;
+			if (object.transform.localPosition.y <= -0.13){
+			object.transform.localPosition.y = -0.13;
+			state = 0;
+			}
+		}
+		if (state == 0){
+			object.transform.localPosition.y += 0.005;
+			if (object.transform.localPosition.y >= 0.13){
+			object.transform.localPosition.y = 0.13;
+			state = 1;
+			}
+		}
+	}
+}
