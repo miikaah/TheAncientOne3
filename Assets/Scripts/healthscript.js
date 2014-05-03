7ae544d439803f5c7f0e7d644c519bf36050a11f
letteralex <letteralex@hotmail.com>
2014-05-03 10:58:42 +0300

    Allu 03.05
    Hp scripti(täl hetkel vähentää 1 hp sekunnin välein testin vuoksi) ja
    gui text

---
+++ Assets/Scripts/healthscript.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+    var curHealth : int = 100;
+    var maxHealth : int = 100;
+    var healthtext : GUIText;
+    
+    function Start () {
+    Regenerate();
+    }
+     
+    function Update () {
+     
+     
+    healthtext.text = "Health: " + curHealth + " / " + maxHealth;
+     
+     
+    if(curHealth < 0 ) {
+    curHealth = 0;
+    }
+     
+    if(curHealth > 100) {
+    curHealth = 100;
+    }
+
+  }
+     
+    function Regenerate () {
+    var i: int;
+    for(i=1;i>0;i++) {
+    yield WaitForSeconds(1);
+     
+    //if(curHealth < maxHealth) {
+    if(curHealth > 0) {
+    curHealth--;
+    }
+     
+     
+    }
+     
+     
+  }
+
